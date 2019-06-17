26e4521ef747c1b5aebce853853383a7b105c53b
Emile Cantin <emile.cantin@gmail.com>
2019-06-17 12:06:27 -0400

    Fixed security issues

--- server/resolvers/index.js
+++ server/resolvers/index.js
@@ -4,9 +4,9 @@ const db = require('../models');
 const resolvers = {
   Query: {
     async house(obj, args) {
-      const houseResult = await db.sequelize.query(`SELECT * FROM Houses WHERE id = ${args.id}`);
-      const pointsResult = await db.sequelize.query(`SELECT SUM(quantity) AS points FROM PointAssignations WHERE house = ${args.id}`);
-      const assignationsResult = await db.sequelize.query(`SELECT * FROM PointAssignations WHERE house = ${args.id}`);
+      const houseResult = await db.sequelize.query(`SELECT * FROM Houses WHERE id = $1`, {bind: [args.id]});
+      const pointsResult = await db.sequelize.query(`SELECT SUM(quantity) AS points FROM PointAssignations WHERE house = $1`, {bind: [args.id]});
+      const assignationsResult = await db.sequelize.query(`SELECT * FROM PointAssignations WHERE house = $1`, {bind: [args.id]});
       return {
         ...houseResult[0][0],
         points: (pointsResult[0][0] && pointsResult[0][0].points) || 0,
